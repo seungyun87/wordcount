c8301d31ca873bcf641016bdc0a9114c6e00a50d
seungyun87 <calebads@likelion.org>
2020-07-18 11:09:26 +0900

    wordcount 1.0

---
+++ firstproject/wordcount/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split()
+    word_dict = {}
+    for i in words:
+        if i in word_dict:
+            word_dict[i]+=1
+        else:
+            word_dict[i]=1
+    return render(request, 'result.html', {'full':text, 'total':len(words), 'dictionary':word_dict.items()})
+
+    
+    
+   
+            
